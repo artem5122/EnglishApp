da22b8ce18015b4034fb79d50d7767d9fde8af1a
artem5122 <119778565+artem5122@users.noreply.github.com>
2023-03-31 10:35:45 +0400

    Add files via upload

---
+++ EnglishApp/EnglishApp/ResultOfTraining.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EnglishApp
+{
+    public partial class ResultOfTraining : Form
+    {
+        public ResultOfTraining()
+        {
+            InitializeComponent();
+        }
+
+        //Первая строка: неправильные слова
+        //Вторая строка: правильные слова
+
+        public List<string> CorrectWords { get; set; }
+        public List<string> IncorrectWords { get; set; }
+
+
+
+        private void ResultOfTraining_Load(object sender, EventArgs e)
+        {
+            string words = "";
+            foreach(string str in IncorrectWords)
+            {
+                words += str+", ";
+            }
+            if (words.Length > 0)
+            {
+                words = words.Remove(words.Length - 2);
+                WordList.Text = ("Вот эти слова тебе стоит подучить: " + words);
+            }
+            else WordList.Text = ("Идёшь без ошибок! \nА ты хорооош \u263A");
+            
+            //Изменяем уровни у слов
+            DictionaryTaking changes = new DictionaryTaking();
+            changes.LevelChange(CorrectWords, IncorrectWords);
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            TrainingForm trainingForm = new TrainingForm();
+            trainingForm.Show();
+        }
+
+        
+    }
+}
